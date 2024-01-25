bc9c514f828e35a30a4a1e1946c69e8624456a25
akouzm <andrew.kouzmenko@gmail.com>
2024-01-25 11:08:39 +1100

    Update PingMonitor.sh
    Determine and use $GATEWAY

--- PingMonitor.sh
+++ PingMonitor.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #echo [$(ping -c 1 192.168.20.1 |grep time= |awk '{print $7}' | cut -f2 -d=)ms]
-IP=192.168.20.1
-#IP=192.168.20.222
+GATEWAY=$(netstat -rn|grep "^0.0.0.0"|awk '{print $2}')
 
-Result=$(ping -w 1 -c 1 $IP |grep time= |awk '{print $7}' | cut -f2 -d=)
+Result=$(ping -w 1 -c 1 $GATEWAY |grep time= |awk '{print $7}' | cut -f2 -d=)
 
 [[ ! -z $Result ]] && echo "[${Result}ms]"
 [[ -z $Result ]] && echo TIMEOUT
